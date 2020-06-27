c7858d15fa9ad6af0ac52634a0c68a36674f726c
mavn <62775874+mavn2@users.noreply.github.com>
2020-06-26 18:09:33 -0700

    char validation check; also  added missing ';'

--- 3hw/script.js
+++ 3hw/script.js
@@ -10,3 +10,8 @@ var specA = ["!","@","#","$","%","^","&","*","(",")","-","+",".","/",",","<",">"
 var numA = ["1","2","3","4","5","6","7","8","9"]
 var lowA = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var uppA = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+//If statement(s) to check for valid user inputs
+if(spec === false && num === false && low === false && upp === false){
+    alert("Please select at least one character type! Please refresh page.")
+}

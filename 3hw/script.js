4246b1d2029fe2c8b712f48dece74be8fca1604b
mavn <62775874+mavn2@users.noreply.github.com>
2020-06-26 18:21:37 -0700

    added check for character #

--- 3hw/script.js
+++ 3hw/script.js
@@ -1,17 +1,23 @@
 //Prompts to determine passwords paramenters
-var length = prompt("Please enter (not type) a number between 8 and 128")
-var spec = confirm("Would you like to include special characters?")
-var num = confirm("Would you like to include numbers?")
-var low = confirm("Would you like to include lowercase letters?")
-var upp = confirm("Would you like to include uppercase letters?")
+var length = prompt("Please enter (not type) a number between 8 and 128");
+var spec = confirm("Would you like to include special characters?");
+var num = confirm("Would you like to include numbers?");
+var low = confirm("Would you like to include lowercase letters?");
+var upp = confirm("Would you like to include uppercase letters?");
 
 //Arrays containing characters
-var specA = ["!","@","#","$","%","^","&","*","(",")","-","+",".","/",",","<",">","~","|","?",":",";","{","}","[","]"]
-var numA = ["1","2","3","4","5","6","7","8","9"]
-var lowA = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var uppA = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var specA = ["!","@","#","$","%","^","&","*","(",")","-","+",".","/",",","<",">","~","|","?",":",";","{","}","[","]"];
+var numA = ["1","2","3","4","5","6","7","8","9"];
+var lowA = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppA = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-//If statement(s) to check for valid user inputs
+//If statement(s) to check for valid user inputs:
+//Makes sure at least one kind of character is selected, and password can be generated
 if(spec === false && num === false && low === false && upp === false){
     alert("Please select at least one character type! Please refresh page.")
+};
+//Converts user input string to number, checks it 
+var lcheck = Number(length)
+if(lcheck < 8 || lcheck >  128){
+    alert("Please input a number (in numeric form) between 8 and 128! Please refresh page.")
 }

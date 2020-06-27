de9e08cfd5b13e4a7348b0c7bd72d97fcd6ad50a
mavn <62775874+mavn2@users.noreply.github.com>
2020-06-26 18:03:47 -0700

    added char arrays, changed hi to upp in var names to better reflect what the opposite of lowercase is

--- 3hw/script.js
+++ 3hw/script.js
@@ -1,5 +1,12 @@
+//Prompts to determine passwords paramenters
 var length = prompt("Please enter (not type) a number between 8 and 128")
 var spec = confirm("Would you like to include special characters?")
 var num = confirm("Would you like to include numbers?")
 var low = confirm("Would you like to include lowercase letters?")
-var hi = confirm("Would you like to include uppercase letters?")
+var upp = confirm("Would you like to include uppercase letters?")
+
+//Arrays containing characters
+var specA = ["!","@","#","$","%","^","&","*","(",")","-","+",".","/",",","<",">","~","|","?",":",";","{","}","[","]"]
+var numA = ["1","2","3","4","5","6","7","8","9"]
+var lowA = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var uppA = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]

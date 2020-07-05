effbc1d5676afa1160b494d03ad4ae9c1d7ea430
mavn <62775874+mavn2@users.noreply.github.com>
2020-07-04 21:46:21 -0700

    added on click for putton, JSON to retrieve array from local storage

--- 4hw/script.js
+++ 4hw/script.js
@@ -1,2 +1,14 @@
-var questions = localStorage.getItem("questions")
-console.log(questions)
+//Allows questions to be quickly accessed from local storage
+var questions = JSON.parse(localStorage.getItem("questions"));
+console.log(questions);//keeping this for testing just in case but don't forget
+
+//Code for button to start quiz
+var startB = document.getElementById('start');
+startB.addEventListener("click",function(){
+    console.log('testing b')
+});
+
+//Function to display quiz in center box
+function renderQuestion(){
+
+};

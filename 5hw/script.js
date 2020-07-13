390604e4b1254232c8d6b74cf9924abd11d8f63c
Max Nicolai <maxvnicolai@gmail.com>
2020-07-13 14:48:56 -0700

    working on time function-foundation vars/functions should be in place

--- 5hw/script.js
+++ 5hw/script.js
@@ -1,9 +1,14 @@
-//Sets time for each row through API
 
+//Current time retrieved through API
 var currentTime = moment();
-console.log(currentTime)
 
-function refresh(time){}
+//Displays current date/time in header
+setHeaderDisplay();
+function setHeaderDisplay(){
+    $("#currentDay").text(currentTime);
+};
+
+//Moment times for each hour
 var hour1 =moment().hour(09).minute(00).second(00);
 var hour2 =moment().hour(10).minute(00).second(00);
 var hour3 =moment().hour(11).minute(00).second(00);
@@ -13,9 +18,23 @@ var hour6 =moment().hour(14).minute(00).second(00);
 var hour7 =moment().hour(15).minute(00).second(00);
 var hour8 =moment().hour(16).minute(00).second(00);
 var hour9 =moment().hour(17).minute(00).second(00);
+var hoursArray = [hour1, hour2, hour3, hour4, hour5, hour6, hour7, hour8, hour9,]
 
-function setTimes(){
-    $("#hour-1").text(hour1)
-    $("#hour-2").text(hour2)
-}
+//Writes times to apropriate rows w/ moment
 setTimes();
+function setTimes(){
+    $("#hour-1").text(hour1.format("hA"))
+    $("#hour-2").text(hour2.format("hA"))
+    $("#hour-3").text(hour3.format("hA"))
+    $("#hour-4").text(hour4.format("hA"))
+    $("#hour-5").text(hour5.format("hA"))
+    $("#hour-6").text(hour6.format("hA"))
+    $("#hour-7").text(hour7.format("hA"))
+    $("#hour-8").text(hour8.format("hA"))
+    $("#hour-9").text(hour9.format("hA")) 
+}
+
+//Gets current time
+//function refresh(time){}
+
+//Checks current time against hours, sets classes accordingly

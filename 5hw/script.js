c3f4717a911901263986c69f7665c485e8bd49e7
Max Nicolai <maxvnicolai@gmail.com>
2020-07-15 11:59:33 -0700

    changed html time block id's to use same array numbering scheme as other elements, reworking setTimes function accordingly. it's much more compact now

--- 5hw/script.js
+++ 5hw/script.js
@@ -24,15 +24,10 @@ var hoursArray = [hour1, hour2, hour3, hour4, hour5, hour6, hour7, hour8, hour9]
 //Writes times to apropriate rows w/ moment
 setTimes();
 function setTimes(){
-    $("#hour-1").text(hour1.format("hA"))
-    $("#hour-2").text(hour2.format("hA"))
-    $("#hour-3").text(hour3.format("hA"))
-    $("#hour-4").text(hour4.format("hA"))
-    $("#hour-5").text(hour5.format("hA"))
-    $("#hour-6").text(hour6.format("hA"))
-    $("#hour-7").text(hour7.format("hA"))
-    $("#hour-8").text(hour8.format("hA"))
-    $("#hour-9").text(hour9.format("hA")) 
+    for(i = 0; i <hoursArray.length; i++){
+        iString = i.toString();
+        $("#hour-" + iString).text(hoursArray[i].format("hA"));
+    };
 }
 
 //Refreshes timer at .5 second intervals

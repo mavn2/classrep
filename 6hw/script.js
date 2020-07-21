24c5876806f2c1ac4746c85c51d28f8acdec5180
Max Nicolai <maxvnicolai@gmail.com>
2020-07-21 15:05:58 -0700

    in progress-code appears solid, but need work around for COR error

--- 6hw/script.js
+++ 6hw/script.js
@@ -1,18 +1,25 @@
 
 //Vars for accessing html
 sBtn = $("#search-btn")
-console.log(sInput)
+sInput = $("#search-inp")
 //Onclick function for search button
-sBtn.on("click", function(){
+sBtn.on("click", function() {
   event.preventDefault()
   searchFn();
 });
 //Function to call search
-function searchFn(){
-  var sTerm = sInput.val();
+function searchFn() {
+  var sTerm = sInput.val().trim();
   var key = "4ad72672269c45f050f13d8aa28bb91b";
   var qURL = "api.openweathermap.org/data/2.5/weather?q=" + sTerm + "&appid=" + key;
   console.log (qURL)
   console.log(sTerm)
+  $.ajax({
+      url: qURL,
+      method: "GET"
+  })
+    .then(function(response) {
+      console.log(response)
+    })
 };
 //Function to display results

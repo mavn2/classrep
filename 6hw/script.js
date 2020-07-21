e3b767a56e2778ed3091bbb16a22b3629cd3091d
Max Nicolai <maxvnicolai@gmail.com>
2020-07-21 15:58:51 -0700

    all info required from response stored in vars

--- 6hw/script.js
+++ 6hw/script.js
@@ -21,7 +21,16 @@ function searchFn() {
   })
     .then(function(response) {
       console.log(response)
+      var name = response.name;
+      var icon = response.weather[0].icon
+      var time = response.dt + response.timezone
+      var temp = Math.round((response.main.temp - 273.15) * 1.8 + 32) + " °F";
+      var hum = response.main.humidity + "%";
+      var wSpd = response.wind.speed + " MPH";
+      var lat = response.coord.lat;
+      var long = response.coord.lon;
+      console.log(temp, name, hum, wSpd, lat, long, icon, time);
     })
 };
 
-//Function to display results
+//Function to display primary forcast

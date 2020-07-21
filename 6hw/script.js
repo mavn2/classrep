05f8336cbd3905cdf1376e753c78cc437139fd25
Max Nicolai <maxvnicolai@gmail.com>
2020-07-21 15:18:09 -0700

    fixed url, query working

--- 6hw/script.js
+++ 6hw/script.js
@@ -1,17 +1,18 @@
-
 //Vars for accessing html
 sBtn = $("#search-btn")
 sInput = $("#search-inp")
+
 //Onclick function for search button
 sBtn.on("click", function() {
   event.preventDefault()
   searchFn();
 });
+
 //Function to call search
 function searchFn() {
   var sTerm = sInput.val().trim();
   var key = "4ad72672269c45f050f13d8aa28bb91b";
-  var qURL = "api.openweathermap.org/data/2.5/weather?q=" + sTerm + "&appid=" + key;
+  var qURL = "https://api.openweathermap.org/data/2.5/weather?q=" + sTerm + "&appid=" + key;
   console.log (qURL)
   console.log(sTerm)
   $.ajax({
@@ -22,4 +23,5 @@ function searchFn() {
       console.log(response)
     })
 };
+
 //Function to display results
